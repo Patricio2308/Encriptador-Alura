033a169d3ba2b06b3c7eb4e0a5797694337022b5
Patricio2308 <patricioandres00@gmail.com>
2023-04-17 19:52:04 -0300

    logica de encriptación

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-html
+var encBtn = document.getElementById("encriptador");
+var desencBtn = document.getElementById("desencriptador");
+var inputText = document.querySelector("input");
+var areaText = document.querySelector("textarea");
+
+var texto = '';
+
+function encriptar() {
+    texto = inputText.value;
+    var frase = "";
+    frase = texto
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+
+    areaText.textContent = frase;
+}
+function desencriptar() {
+    texto = inputText.value;
+    var frase = '';
+
+    frase = texto
+        .replace(/ufat/g, 'u')
+        .replace(/ober/g, 'o')
+        .replace(/ai/g, 'a')
+        .replace(/imes/g, 'i')
+        .replace(/enter/g, 'e');
+
+    areaText.textContent = frase;
+}
+/* 
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+
+encBtn.onclick = encriptar;
+desencBtn.onclick = desencriptar;
